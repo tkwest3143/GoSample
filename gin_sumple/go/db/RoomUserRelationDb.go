8a475e43ff3a29f7277160b0f093d808a3fedc94
SuperUser <47804655+tkwest3143@users.noreply.github.com>
2020-10-14 00:02:02 +0900

    update

--- gin_sumple/go/db/RoomUserRelationDb.go
+++ gin_sumple/go/db/RoomUserRelationDb.go
@@ -4,9 +4,27 @@ import (
 	"github/GoSumple/gin_sumple/go/data"
 )
 
-//RoomUserRelationInsert users情報を挿入します
+//RoomUserRelationInsert RoomUserRelations情報を挿入します
 func RoomUserRelationInsert(insData data.RoomUserRelation) {
 	d := GormConnect()
 	d.Create(&insData)
 	defer d.Close()
+}
+
+//RoomUserRelationSelectByUserID ユーザIDをもとにRoomUserRelations情報を取得します
+func RoomUserRelationSelectByUserID(userID string) data.RoomUserRelation {
+	d := GormConnect()
+	selData := data.RoomUserRelation{}
+	d.First(&selData, "user_id=?", userID)
+	defer d.Close()
+	return selData
+}
+
+//RoomUserRelationSelectByRoomID ルームIDをもとにRoomUserRelations情報を取得します
+func RoomUserRelationSelectByRoomID(roomID string) []data.RoomUserRelation {
+	d := GormConnect()
+	selData := []data.RoomUserRelation{}
+	d.First(&selData, "room_id=?", roomID)
+	defer d.Close()
+	return selData
 }
