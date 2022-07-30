5ebf0e396c8047db6b7bb4438cfa984c6b7652d8
Nishi Takanori <47804655+tkwest3143@users.noreply.github.com>
2022-07-30 23:46:21 +0900

    update

--- strike/go/common/CryptUtility.go
+++ strike/go/common/CryptUtility.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"crypto/rand"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -10,4 +11,14 @@ func DoCrypt(str string) string {
 	hash, _ := bcrypt.GenerateFromPassword([]byte(str), 12)
 
 	return string(hash)
+}
+
+// ランダム文字列作成
+func GetRandom() (string, error) {
+	b := make([]byte, 64)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return string(b), err
 }
